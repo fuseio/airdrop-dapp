150d04873c6ed51ac05b67c84381907cb366d141
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-03-25 11:48:34 +0000

    Prepare access to all functionalities to transfer to QA team

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -9,12 +9,11 @@ type NavButtonProps = {
 const NavButton = ({ isOpen, setOpen }: NavButtonProps) => {
   return (
     <div className="flex order-2 min-w-[150px] md:w-[93%] justify-end items-center">
-      {/* ConnectWallet is commented out for Airdrop website Coming Soon */}
-      {/* <ConnectWallet
+      <ConnectWallet
         containerClassName="ml-auto md:me-3"
         className="transition ease-in-out hover:bg-success hover:text-black hover:border-success"
         location="navbar"
-      /> */}
+      />
       <button
         type="button"
         className="p-2 w-10 h-8 hidden md:inline-flex focus:outline-none"
