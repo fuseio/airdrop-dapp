980c2b5a1d3b00664796cdf85754be9bcfea4682
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-03-27 13:45:25 +0000

    Fix Eli mobile and big screen broken responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -42,14 +42,14 @@ export default function Airdrop() {
   }, [isDisconnected, dispatch])
 
   return (
-    <div className={`w-full font-mona bg-secondary ${currentComponent === "landing" ? "h-screen md:min-h-[1500px]" : "min-h-screen bg-radial-gradient-green"}`}>
+    <div className={`w-full font-mona bg-secondary ${currentComponent === "landing" ? "h-screen" : "min-h-screen bg-radial-gradient-green"}`}>
       <div className={`flex-col flex justify-start items-center bg-cover bg-no-repeat bg-bottom ${currentComponent === "landing" ? "h-screen" : "min-h-screen bg-[url('/vectors/grid.svg')]"}`}>
         {currentComponent === "landing" &&
           <div className="absolute w-full h-full z-0 min-h-[800px] bg-secondary overflow-hidden">
             <div className="absolute w-full h-full z-[4] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer1.svg')] bg-no-repeat bg-[center_bottom] bg-100% animate-slide-in-bg-layer1"></div>
-            <div className="absolute w-full h-full z-[3] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer2.svg')] bg-no-repeat bg-[left_86%_bottom_calc((43vw-1400px)*0.08)] bg-80% animate-slide-in-bg-layer2 translate-y-1/2 md:bg-[left_86%_bottom_calc((50vw-275px)*0.08)]"></div>
+            <div className="absolute w-full h-full z-[3] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer2.svg')] bg-no-repeat bg-[left_86%_bottom] bg-80% animate-slide-in-bg-layer2 translate-y-1/2 md:bg-[left_86%_bottom]"></div>
             <div className="absolute w-full h-full z-[2] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer3.svg')] bg-no-repeat bg-[left_calc(50vw-591px)_bottom_calc((50vw-400px)*0.2)] animate-slide-in-bg-layer3 translate-y-1/2"></div>
-            <div className="absolute w-full h-full z-[1] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer4.svg')] bg-no-repeat bg-[center_bottom_calc(28vh*2.2-495px)] bg-contain max-w-[800px] left-[calc(50%-400px)] animate-slide-in-bg-layer4 translate-y-[60%] md:max-w-full md:left-0"></div>
+            <div className="absolute w-full h-full z-[1] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer4.svg')] bg-no-repeat bg-[center_bottom_20vh] bg-contain max-w-[800px] left-[calc(50%-400px)] animate-slide-in-bg-layer4 translate-y-[80%] 4xl:bg-[center_bottom_12vh] 2xl:bg-[center_bottom_4vh] lg:bg-[center_bottom_10vh] md:max-w-full md:left-0"></div>
             <div className="absolute w-full h-full z-[5] min-h-[800px] overflow-hidden bg-[url('/vectors/bg-layer5.svg')] bg-no-repeat bg-[center_top] bg-auto-100% animate-slide-in-bg-layer5 translate-y-1/2"></div>
             <div className="absolute w-full h-full z-[1] min-h-[800px] overflow-hidden bg-[url('/vectors/stars1.svg')] bg-no-repeat bg-[center_0] animate-slide-in-bg-stars1 translate-y-1/2"></div>
             <div className="absolute w-full h-full z-[1] min-h-[800px] overflow-hidden bg-[url('/vectors/stars2.svg')] bg-no-repeat bg-[center_0] animate-slide-in-bg-stars2 translate-y-1/2"></div>
