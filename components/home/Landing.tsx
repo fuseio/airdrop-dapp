80b6aef5e18a93921793106ac30830f94f48e3bd
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-04-10 10:10:33 +0000

    Change coming soon to the main version

--- components/home/Landing.tsx
+++ components/home/Landing.tsx
@@ -6,12 +6,6 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useAccount } from "wagmi";
 import JoinAirdrop from "./JoinAirdrop";
-import { NEXT_PUBLIC_ENVIRONMENT } from "@/lib/config";
-import Countdown from "./Countdown";
-import NotifySubscribe from "./NotifySubscribe";
-import { selectNavbarSlice, setCurrentComingSoonComponent } from "@/store/navbarSlice";
-import rightArrow from "@/assets/right-arrow.svg";
-import Image from "next/image";
 
 const Landing = () => {
   const dispatch = useAppDispatch();
@@ -19,7 +13,6 @@ const Landing = () => {
   const searchParams = useSearchParams();
   const referralCode = searchParams.get('ref');
   const { isHydrated, isAuthenticated, isUser } = useAppSelector(selectUserSlice);
-  const { currentComingSoonComponent } = useAppSelector(selectNavbarSlice);
   const { address } = useAccount();
 
   useEffect(() => {
@@ -56,52 +49,12 @@ const Landing = () => {
         You are invited to the Fuse Airdrop! Don&apos;t be left out of the chance to explore,
         get points and earn. We are thrilled to have you as a part of the Fuse vibrant community.
       </p>
-      {NEXT_PUBLIC_ENVIRONMENT === "staging" &&
-        <div className="flex flex-col gap-3.5">
-          <p className="text-xl text-white font-bold mb-5 2xl:hidden">
-            Enter invite code
-          </p>
-          <JoinAirdrop invite={invite} setInvite={setInvite} />
-        </div>
-      }
-      {(NEXT_PUBLIC_ENVIRONMENT === "production" && currentComingSoonComponent === "countdown") &&
-        <motion.div
-          className="flex flex-col gap-10 3xl:gap-6 md:gap-4"
-          key="countdown"
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: -300, opacity: 0 }}
-        >
-          <Countdown />
-          <button
-            className="group flex justify-center items-center gap-1 2xl:mt-1.5"
-            onClick={() => dispatch(setCurrentComingSoonComponent("notify"))}
-          >
-            <p className="text-xl md:text-base text-white font-bold">
-              Get notified when the airdrop is live
-            </p>
-            <Image
-              src={rightArrow}
-              alt="right arrow"
-              className="transition ease-in-out group-hover:translate-x-0.5"
-            />
-          </button>
-        </motion.div>
-      }
-      {(NEXT_PUBLIC_ENVIRONMENT === "production" && currentComingSoonComponent === "notify") &&
-        <motion.div
-          className="flex flex-col gap-3.5"
-          key="notify"
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: -300, opacity: 0 }}
-        >
-          <p className="text-xl text-white/70 font-bold 2xl:hidden">
-            Get notified when the airdrop is live
-          </p>
-          <NotifySubscribe />
-        </motion.div>
-      }
+      <div className="flex flex-col gap-3.5">
+        <p className="text-xl text-white font-bold mb-5 2xl:hidden">
+          Enter invite code
+        </p>
+        <JoinAirdrop invite={invite} setInvite={setInvite} />
+      </div>
     </motion.div>
   )
 }
