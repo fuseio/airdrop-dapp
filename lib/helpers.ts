5fd7c58c32a84f9d8ed7324ea7b23ed7062c8dd9
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-05-29 12:59:43 +0000

    Add Metamask SDK in Wagmi

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -16,6 +16,7 @@ export const IS_SERVER = typeof window === "undefined";
 
 export const walletType: WalletType = {
   "injected": "MetaMask",
+  "metaMaskSDK": "MetaMaskSDK",
   "walletConnect": "WalletConnect",
   "coinbaseWallet": "Coinbase",
   "google": "Google",
@@ -27,7 +28,17 @@ export const walletType: WalletType = {
   "email_passwordless": "Email"
 }
 
-export const isIos = !IS_SERVER && /iPad|iPhone|iPod/.test(navigator.userAgent);
+export const detectDevice = () => {
+  if (IS_SERVER) {
+    return { isIos: null, isAndroid: null, isMobile: null };
+  }
+
+  const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/.test(navigator.userAgent);
+  const isMobile = isIos || isAndroid;
+
+  return { isIos, isAndroid, isMobile };
+}
 
 export const path = {
   HOME: "/",
