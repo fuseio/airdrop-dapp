2ec4b370f458ece223de7ebf7cca6db931dbb2f8
MusabShakeel576 <46605319+MusabShakeel576@users.noreply.github.com>
2024-07-17 13:55:52 +0000

    Reduce boost modal size

--- components/home/BoostModal.tsx
+++ components/home/BoostModal.tsx
@@ -41,18 +41,18 @@ const BoostModal = (): JSX.Element => {
               <Image
                 src={boostedQuests}
                 alt="boosted quests"
-                width={1080}
-                height={1080}
+                width={600}
+                height={600}
               />
-              <div className="absolute bottom-[14%] left-1/2 -translate-x-1/2 w-full max-w-2xl 3xl:max-w-md text-center">
-                <p className="text-2xl 3xl:text-base md:text-sm text-white">
+              <div className="absolute bottom-[14%] left-1/2 -translate-x-1/2 w-full max-w-md text-center">
+                <p className="md:text-sm leading-5 text-white">
                   You can now earn even more points by providing liquidity
                   on the Fuse network through boosted quests. Get{" "}
                   <span className="font-bold">12 points</span>{" "}
                   instead of 8 for every $1 bridged.
                 </p>
                 <button
-                  className="transition ease-in-out bg-primary border border-primary rounded-full text-lg xl:text-sm text-black leading-none font-bold mt-2 md:mt-1 px-7 py-3 xl:px-4 xl:py-1.5 md:py-1 hover:bg-transparent hover:text-primary"
+                  className="transition ease-in-out bg-primary border border-primary rounded-full text-lg xl:text-sm text-black leading-none font-bold mt-2 md:mt-1 px-7 py-2 xl:px-4 xl:py-1.5 md:py-1 hover:bg-transparent hover:text-primary"
                   onClick={() => {
                     if (checked) {
                       localStorage.setItem("airdrop-isBoostedModalHidden", "true");
@@ -63,12 +63,12 @@ const BoostModal = (): JSX.Element => {
                   Got it
                 </button>
               </div>
-              <label htmlFor="hide-boost-modal" className="flex items-center gap-2.5 relative mt-11 3xl:mt-6 text-2xl 3xl:text-base text-white leading-none">
+              <label htmlFor="hide-boost-modal" className="flex items-center gap-2.5 relative mt-6 text-white leading-none">
                 <input
                   type="checkbox"
                   id="hide-boost-modal"
                   name="hide-boost-modal"
-                  className="boost-modal-checkbox appearance-none w-6 h-6 3xl:w-4 3xl:h-4 bg-transparent border border-success rounded text-success"
+                  className="boost-modal-checkbox appearance-none w-4 h-4 bg-transparent border border-success rounded text-success"
                   checked={checked}
                   onChange={() => setChecked(!checked)}
                 />
